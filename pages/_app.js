52e1218bf391cad4a3b325a63a87dee8edb12437
Khaled Ali <Khaledvsall@gmail.com>
2022-09-14 00:01:38 -0700

    port over stopwatch code from local react app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,103 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { createRef, useEffect, useState } from "react";
+import "../styles/App.css";
+
+function App() {
+  const [secondsLeft, setSecondsLeft] = useState(0);
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerPaused, setTimerPaused] = useState(false);
+  const [timerInput, setTimerInput] = useState("0:00");
+
+  const timeLabel = () => {
+    return `${parseInt(secondsLeft / 60)}:${secondsLeft % 60 < 10 ? "0" : ""}${
+      secondsLeft % 60 === 0 ? "0" : secondsLeft % 60
+    }`;
+  };
+
+  const timer = createRef();
+
+  const handleTimerInputChange = (e) => {
+    if (timerStarted) return;
+    let result = e.target.value.split(":").join("");
+    let newMinutes =
+      result.slice(0, -2)[0] === "0"
+        ? result.slice(1, -2)
+        : result.slice(0, -2);
+    result = `${newMinutes ? newMinutes : "0"}:${
+      result.slice(-2)?.length === 2 ? result.slice(-2) : "00"
+    }`;
+    setTimerInput(`${result}`);
+  };
+
+  const startTimer = () => {
+    setTimerStarted(true);
+    if (timerPaused) {
+      setTimerPaused(false);
+    } else {
+      const [minutes, seconds] = timerInput.split(":");
+      const totalSecondsLeft = parseInt(minutes) * 60 + parseInt(seconds);
+      setSecondsLeft(totalSecondsLeft);
+    }
+  };
+
+  const stopTimer = () => {
+    setTimerStarted(false);
+    setTimerPaused(false);
+    setSecondsLeft(0);
+  };
+
+  const pauseTimer = () => {
+    setTimerPaused(true);
+    clearTimeout(timer.current);
+  };
+
+  useEffect(() => {
+    if (secondsLeft > 0 && timerStarted && !timerPaused) {
+      timer.current = setTimeout(() => {
+        setSecondsLeft(secondsLeft - 1);
+      }, 1000);
+    } else if (secondsLeft === 0 && timerStarted) {
+      pauseTimer();
+    }
+  }, [timerStarted, secondsLeft, timer]);
+
+  const timerEnded = timerPaused && secondsLeft === 0;
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        Scrum Watch
+        <input
+          className="timer"
+          value={timerStarted ? timeLabel() : timerInput}
+          onChange={handleTimerInputChange}
+        ></input>
+        <div className="button-container">
+          <button
+            onClick={startTimer}
+            disabled={(timerStarted && !timerPaused) || timerEnded}
+          >
+            {timerPaused ? "resume timer" : "start timer"}
+          </button>
+          <button
+            id="stop-btn"
+            onClick={stopTimer}
+            disabled={!timerStarted && !timerPaused}
+          >
+            stop/reset timer
+          </button>
+          <button
+            id="pause-btn"
+            onClick={pauseTimer}
+            disabled={!timerStarted || timerPaused}
+          >
+            pause timer
+          </button>
+        </div>
+      </header>
+    </div>
+  );
 }
 
-export default MyApp
+export default App;
